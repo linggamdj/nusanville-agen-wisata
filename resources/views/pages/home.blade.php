ef150f745f312f36c1d3bf4dd5ac77a7b6405685
Lingga Adi Atmadja <linggaadiatmdj@gmail.com>
2021-06-09 17:25:37 +0700

    home updated

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,9 +9,9 @@ NUSANVILLE
 <!-- HEADER -->
 
 <header class="text-center">
-    <h1>Explore The Beautiful World<br>As Easy One Click</h1>
-    <p class="mt-3">You will see beautiful<br>moment you never see before</p>
-    <a href="#popular" class="btn btn-get-started px-4 mt-4">
+    <h1>Menelusuri Wisata Tersembunyi<br>Bersama Nusanville</h1>
+    {{-- <p class="mt-3">You will see beautiful<br>moment you never see before</p> --}}
+    <a href="#popular" class="btn btn-get-started px-4 mt-5">
         Get Started
     </a>
 </header>
@@ -46,7 +46,7 @@ NUSANVILLE
         <div class="container">
             <div class="row">
                 <div class="col text-center section-popular-heading">
-                    <h2>Wisata Popular</h2>
+                    <h2>Wisata Terkini</h2>
                     <p>Something that you never try<br>before in this world</p>
                 </div>
             </div>
@@ -93,14 +93,14 @@ NUSANVILLE
         <div class="container">
             <div class="row">
                 <div class="col text-center">
-                    <h2>They Are Loving Us</h2>
-                    <p>Moments were giving them<br>the best experience</p>
+                    <h2>Apa Kata Mereka?</h2>
+                    <p>Testimonial dari mereka<br>yang telah berwisata bersama kami</p>
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="section section-testimonial-content" id="testimonial-content">
+    {{-- <section class="section section-testimonial-content" id="testimonial-content">
         <div class="container">
             <div class="section-popular-travel row justify-content-center">
                 <div class="col-sm-6 col-md-6 col-lg-4">
@@ -165,8 +165,84 @@ NUSANVILLE
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 
+    <section class="section section-testimonial-content" id="testimonial-content">
+        <div class="container">
+            <div class="carousel slide section-popular-travel row justify-content-center" id="carouselExampleControls" data-ride="carousel" data-interval="1500">
+                <div class="carousel-inner col-sm-6 col-md-6 col-lg-4">
+                    <div class="carousel-item active">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <img src="frontend/images/testi-1.png" alt="User" class="mb-4 rounded-circle">
+                                <h3 class="mb-4">Lingga Adi Atmadja</h3>
+                                <p class="testimonial">"It was glorious and I could
+                                    not stop to say wohooo for
+                                    every single moment
+                                    Dankeee!"
+                                </p>
+                            </div>
+                            <hr>
+                            <p class="trip-to mt-2">
+                                Trip to Ubud
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <img src="frontend/images/testi-2.png" alt="User" class="mb-4 rounded-circle">
+                                <h3 class="mb-4">Ekomon Tomoyasu</h3>
+                                <p class="testimonial">"The trip was amazing and
+                                    I saw something beautiful in
+                                    that Island that makes me
+                                    want to learn more!"
+                                </p>
+                            </div>
+                            <hr>
+                            <p class="trip-to mt-2">
+                                Trip to Nusa Penida
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <img src="frontend/images/testi-3.png" alt="User" class="mb-4 rounded-circle">
+                                <h3 class="mb-4">Bligungblog</h3>
+                                <p class="testimonial">"I loved it when the waves
+                                    was shaking harder - I was
+                                    scared too"
+                                </p>
+                            </div>
+                            <hr>
+                            <p class="trip-to mt-2">
+                                Trip to Karimun Jawa
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center">
+                    <a href="#" class="btn btn-help px-4 mt-4 mx-1">
+                        I Need Help
+                    </a>
+                    <a href="{{route('register')}}" class="btn btn-get-started px-4 mt-4 mx-1">
+                        Get Started
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <!-- END OF MAIN -->
